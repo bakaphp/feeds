cbc2da994b9cd0cc22e3e95a4bcd1368e211f6e5
Angel Leger <angel.leger89@gmail.com>
2019-02-08 11:04:07 -0400

    added 3 methods addMessage, addResource, addComment

--- src/Feeds.php
+++ src/Feeds.php
@@ -2,7 +2,12 @@
 
 namespace Baka\Feeds;
 
-use Baka\Feeds\Models\Messages as MessagesModel;
+
+use Baka\Feeds\Models\MessageComments;
+use Baka\Feeds\Models\MessageResources;
+use Baka\Feeds\Models\Messages as MessageModel;
+use Baka\Feeds\Models\MessageResources as MessageResourcesModel;
+
 
 /**
  * \Phalcon\feeds
@@ -16,11 +21,25 @@ class Feeds
 {
 
     /**
-     * The Content for the message
+     * The apps ID for the message
      *
-     * @var string
+     * @var integer
      */
-    private $content;
+    private $apps_id;
+
+    /**
+     * The company ID for the message
+     *
+     * @var integer
+     */
+    private $companies_id;
+
+    /**
+     * The user ID for the message
+     *
+     * @var integer
+     */
+    private $users_id;
 
     /**
      * The Message Type for the message
@@ -30,54 +49,97 @@ class Feeds
     private $message_types_id;
 
     /**
+     * The Content for the message
+     *
+     * @var string
+     */
+    private $context;
+
+    /**
+     * The count of reactions in the message
+     *
+     * @var integer
+     */
+    private $reactions_count;
+
+    /**
+     * The the coutn of comments for the message
+     *
+     * @var integer
+     */
+    private $comments_count;
+
+
+    /**
      * Feeds constructor.
      */
     public function __construct()
     {
-    }
 
+    }
 
     /**
      *
      * Add a new message to the feed.
      *
-     * @param object $user post data.
-     * @param array $request post data.
-     *
-     * @throws \InvalidArgumentException
+     * @param object $user object info.
+     * @param array $request[] post data.
+     *    $request = [
+     *      'type'     => (integer) type of message. Required.
+     *      'message' => (string) the message itself.
+     *    ]
      *
      * @return \Response
      */
-    public function add($user, $request): ?array
+    public function addMessage(object $user, array $request): ?object
     {
 
-        $this->message_types_id = $request['type'];
-        $this->content = $this->getMessageType();
-
-        $request['messageText'] = $this->content;
-
-        $response = MessagesModel::add($user, $request);
-
+        $response = MessageModel::add($user, $request);
 
         return $response;
     }
+
     /**
      *
-     * Get Message Type.
+     * Add a new resource to the feed.
      *
-     * @param object $type
-     *
-     * @throws \InvalidArgumentException
+     * @param object $user object info.
+     * @param array $request[] post data.
+     *    $request = [
+     *      'messages_id'     => (integer) Message ID key. Required.
+     *      'comments_id' => (integer) Comment ID foreign key. Required.
+     *      'media_resources_id' => (integer) Media Resource ID foreign key. Required.
+     *    ]
      *
      * @return \Response
      */
-    public function getMessageType(): ?array
+    public function addResource(object $user, array $request): ?object
     {
 
-        $content = Type::getType($this->message_types_id, $this->content);
+        $response = MessageResourcesModel::add($user, $request);
 
+        return $response;
+    }
 
-        return $content;
+    /**
+     *
+     * Add a new comment to a message in the feed.
+     *
+     * @param object $user object info.
+     * @param array $request[] post data.
+     *    $request = [
+     *      'messages_id'     => (integer) Message ID key. Required.
+     *      'content' => (integer) Content of the comment. Required.
+     *    ]
+     *
+     * @return \Response
+     */
+    public function addComment(object $user, array $request): ?object
+    {
+
+        $response = MessageComments::add($user, $request);
+
+        return $response;
     }
 
 }
